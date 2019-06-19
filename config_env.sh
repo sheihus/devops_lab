b171cf2ee5dd7f414a377a3c04827b5fa840cef0
Andrei Sheihus <sheihus@mail.com>
2019-06-19 15:09:12 +0300

    Conf env v.0.0.3

--- config_env.sh
+++ config_env.sh
@@ -2,25 +2,8 @@
 
 curl  https://pyenv.run | bash
 
-CONFIG=`realpath ~/.bashrc`
-IFS=$'\n'
-PYENVRC='pyenv.rc'
+pyenv install 2.7.0
+pyenv virtualenv 2.7.0 python2
+pyenv install 3.7.0
+pyenv virtualenv 3.7.0 python3
 
-for str in $(cat $PYENVRC)
-do
-  grep -q $str $CONFIG || echo $str >> $CONFIG
-done
-
-source $CONFIG
-
-pyenv install 2.7.16
-pyenv virtualenv 2.7.16 python2
-pyenv install 3.6.8
-pyenv virtualenv 3.6.8 python3
-
-#testing python environment
-pyenv activate python2
-python -V
-pyenv activate python3
-python -V
-pyenv deactivate
