a365274890a503c0a01ed605fe7407ef2f91e5e9
Andrei Sheihus <sheihus@mail.com>
2019-06-19 15:30:17 +0300

    Conf env v.0.0.5

--- install_p.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl  https://pyenv.run | bash
-echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
